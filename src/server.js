2f95e4aa52b1689d8e0c8cf0d799f78846d09ea6
Ahmad Fillah Alwy <ahmadfillahalwy12@gmail.com>
2025-12-12 01:15:44 +0700

    Create repository notes app

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
+require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
-const NotesService = require('./services/inMemory/NotesService');
+const NotesService = require('./services/postgres/NotesService');
+const NotesValidator = require('./validator/notes/index');
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
@@ -18,8 +21,26 @@ const init = async () => {
     plugin: notes,
     options:{
       service: notesService,
+      validator: NotesValidator,
     },
   })
+
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
+  
+    // penanganan client error secara internal.
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+      
+    return h.continue;
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
